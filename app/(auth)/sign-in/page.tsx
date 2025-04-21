867251399786d4c263b4b5b6337b5a123caffbf8
atif-maxgo123 <atif.maxgo@gmail.com>
2025-04-21 11:45:55 +0500

    create auth page functionality done

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,8 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const page = () => {
-  return <div>Sign In Page</div>;
+  return <AuthForm type="sign-in" />;
 };
 
 export default page;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-
+import AuthForm from "@/components/AuthForm";
 const page = () => {
-  return <div>Sign Up</div>;
+  return <AuthForm type="sign-up" />;
 };
 
 export default page;
